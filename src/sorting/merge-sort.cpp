644d00b692f7ec71f25da943f552e16e3f56beab
omaralsayed <42531544+omaralsayed@users.noreply.github.com>
2019-09-26 02:02:47 -0400

    Update comments

--- src/sorting/merge-sort.cpp
+++ src/sorting/merge-sort.cpp
@@ -1,29 +1,32 @@
-/*
-Time complexity: O(nlogn)
-Space complexity: O(n)
+/** 
+ * Reference:
+ * GeeksforGeeks (geeksforgeeks.org)
+ */
+
+/**
+ * Time complexity: O(nlog(n))
+ * Space complexity: O(n)
 */
 
-/*
-Divide and conquer:
-- Take the list and divide it into two halves recursivly
-- Solve the subproblems (have the function call 
-iteself on each half)
-- Combine the solutions of the subproblems to get the 
-result (merge)
-
-Our final result will have a sum of n+...+(n(n/n))
+/**
+ * Merge Sort is a Divide and Conquer algorithm. It divides input
+ * array in two halves, calls itself for the two halves and then 
+ * merges the two sorted halves. The merge() function is used for 
+ * merging two halves. The merge(a, l, m, r) is key process that 
+ * assumes that a[l .. m] and a[m + 1 .. r] are sorted and merges 
+ * the two sorted sub-arrays into one.
 */
 
-void merge(int data[], int l, int m, int r) {
+void merge(int a[], int l, int m, int r) {
 	int s1 = m - l + 1;
 	int s2 = r - m;
 	int* l1 = new int[s1 + 1];
 	int* l2 = new int[s2 + 1];
 
 	for (int i = 0; i < s1; ++i)
-		l1[i] = data[l + i];
+		l1[i] = a[l + i];
 	for (int j = 0; j < s2; ++j)
-		l2[j] = data[m + j + 1];
+		l2[j] = a[m + j + 1];
 
 	// https://en.wikipedia.org/wiki/Sentinel_value
 	l1[s1] = INT_MAX;
@@ -34,9 +37,9 @@ void merge(int data[], int l, int m, int r) {
 
 	for (int i = l; i <= r; ++i) {
 		if (l1[j] < l2[k]) 
-            data[i] = l1[j++];
+            a[i] = l1[j++];
 		else 
-            data[i] = l2[k++];
+            a[i] = l2[k++];
 	}
 
 	delete[] l1;
@@ -44,12 +47,11 @@ void merge(int data[], int l, int m, int r) {
 }
 
 void mergeSort(int array[], int l, int r) {
-
     // Sort array if the right index has a greater value than the left index
 	if (r > l) {
 		int m = (r - l) / 2 + l;    // Integer overflow-safe approach
-		mergeSort(array, l, m);		// Sorts the first half
-		mergeSort(array, m + 1, r); // Sorts the second half
-		merge(array, l, m, r);		// Merges sorted halves
+		mergeSort(array, l, m);		// Sort the first half
+		mergeSort(array, m + 1, r); // Sort the second half
+		merge(array, l, m, r);		// Merge sorted halves
 	}
 }
--- src/sorting/quick-sort.cpp
+++ src/sorting/quick-sort.cpp
@@ -1,44 +1,69 @@
-/* 
-Time complexity: O(n^2)
-Space complexity: O(log(n))
+/** 
+ * Reference:
+ * Introduction to Algorithms (CLRS)
+ */
+
+/**
+ * Time complexity: O(n^2)
+ * Space complexity: O(log(n))
 */
 
-/*
-This algorithm works by picking a midpoint and having the 
-function call itself on each half until the array is sorted
-*/
+/**
+ * The quicksort algorithm has a worst-case running time of O(n^2) on an input
+ * array of n numbers. Despite this slow worst-case running time, quicksort is
+ * often the best practical choice for sorting because it is remarkably efficient
+ * on the average: it's expected running time is O(nlog(n)) and the constant factors
+ * hidden in the O(nlog(n)) notation are quite small. It also has the advantage of 
+ * sorting in place, and it works well even in virtual-memory environments.
+ */
 
-void quickSort(int data[], int l, int r) {
-	int i = l; // i starts at the left index, l
-	int j = r; // j starts at the right index, r
-	int temp;
-	int pivot = data[l + ((r - l) / 2)]; // Integer overflow-safe approach
+/**
+ * The following partition always produces a 9-to-1 split yielding a run-time of 
+ * O(nlog(n)). Nodes represent subproblem sizes and per level cost on the right.
+ * 
+ * 							n ----------------------------------->   (cn)
+ * 					     *      *
+ * 				 (1/10)n         (9/10)n ------------------------>   (cn)
+ *              *       *       *       *
+ *          (1/100)n (9/100)n (9/100)n (81/100)n ---------------->   (cn)
+ *          *      * *      * *      * *       *
+ *                                (81/1000)n (729/1000)n -------->   (cn)
+ *                                *        *   *        *
+ * 								*   *     * * * *      * * ------>  (<= cn)
+ *                                                         *
+ *                                                          1 --->  (<= cn)
+ *                                                                 ----------
+ *                                                                 O(nlog(n))
+ * Recurrence -> T(n) = T(9n/10) + T(n/10) + (cn)
+ */
+
+void quickSort(int a[], int l, int r) {
+	int i = l;
+	int j = r;
+	int tmp;
+	int pivot = a[l + ((r - l) / 2)]; // Integer overflow-safe approach
 
 	while (i <= j) {
-		while (data[i] < pivot) {
-			i++; // Increment index
-		}
+		while (a[i] < pivot)
+			i++;
 
-		while (data[j] > pivot) {
-			j--; // Decrement index
-		}
+		while (a[j] > pivot)
+			j--;
 
         // If elements are unsorted:
 		// swap then update indices
 		if (i <= j) {
-			temp = data[i];
-			data[i] = data[j];
-			data[j] = temp;
+			tmp = a[i];
+			a[i] = a[j];
+			a[j] = tmp;
 			i++;
 			j--;
 		}
 	}
 
-	if (l < j) {
-		quickSort(data, l, j); // Recursively sort the first section
-	}
+	if (l < j)
+		quickSort(a, l, j); // Recursively sort the first half
 
-	if (i < r) {
-		quickSort(data, i, r); // Recursively sort the second section
-	}
+	if (i < r)
+		quickSort(a, i, r); // Recursively sort the second half
 }
